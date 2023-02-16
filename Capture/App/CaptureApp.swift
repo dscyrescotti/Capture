d93c026f09f97d5d84bdf5fbc71c1b5d543f09f7
dscyrescotti <dscyrescotti@gmail.com>
2023-02-16 11:07:48 +0630

    feat: commit initailly

---
+++ Capture/App/CaptureApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaptureApp.swift
+//  Capture
+//
+//  Created by Aye Chan on 2/16/23.
+//
+
+import SwiftUI
+
+@main
+struct CaptureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("Camera View is Coming Soon")
+        }
+    }
+}
